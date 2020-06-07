bd72034e1c8a63f38f408d0ae661417ee8d54427
xWillQ <pasha.shevchenko.90@gmail.com>
2020-06-07 14:32:38 +0700

    Added new migrations

---
+++ database/migrations/2020_06_07_070740_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('login');
+            $table->string('name')->nullable();
+            $table->string('email');
+            $table->string('image')->nullable();
+            $table->string('about')->nullable();
+            $table->string('type')->nullable();
+            $table->string('github')->nullable();
+            $table->string('city')->nullable();
+            $table->boolean('is_finished')->default(0);
+            $table->string('phone')->nullable();
+            $table->date('birthday')->nullable();
+            $table->unsignedBigInteger('position_id');
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->dateTime('adopted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
