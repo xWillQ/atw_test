9cb8f825e5d65ee828a931e7a5e2bc178ada7f52
xWillQ <pasha.shevchenko.90@gmail.com>
2020-06-10 22:11:46 +0700

    Updated controllers to use new models

--- app/Http/Controllers/DepartmentsController.php
+++ app/Http/Controllers/DepartmentsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Resources\DepartmentRes;
 use App\Http\Resources\DepartmentWithPositionsRes;
 use App\Models\Department;
-use App\Models\Position;
 use Illuminate\Http\Request;
 
 class DepartmentsController extends Controller
@@ -16,8 +15,7 @@ class DepartmentsController extends Controller
             return DepartmentRes::collection(Department::all());
         }
         if ($user->role->name == 'worker') {
-            $dep_id = Position::find($user->position_id)->department_id;
-            return DepartmentWithPositionsRes::collection([Department::find($dep_id)]);
+            return DepartmentWithPositionsRes::collection([$user->position->department]);
         }
         if ($user->role->name == 'admin') {
             return DepartmentWithPositionsRes::collection(Department::all());
--- app/Http/Controllers/WorkersController.php
+++ app/Http/Controllers/WorkersController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\WorkersRequest;
 use App\Http\Resources\UserWorkerRes;
-use App\Models\Position;
-use App\Models\Role;
 use App\Models\User;
 use App\Models\UserCard;
-use Illuminate\Http\Request;
 
 class WorkersController extends Controller
 {
@@ -30,8 +27,7 @@ class WorkersController extends Controller
             ->select('users.*', 'department_id'); // Выбираем все столбцы из таблицы пользователей и столбец с id отделов
 
         if ($user->role->name == 'worker') {
-            $user_dep_id = Position::find($user->position_id)->department_id;
-            $query = $query->where('department_id', $user_dep_id);
+            $query = $query->where('department_id', $user->position->department);
         }
 
         if ($name != null) {
@@ -59,7 +55,7 @@ class WorkersController extends Controller
 
         if (
             $user->role->name == 'worker' &&
-            $worker->position->department->name != $user->position->department->name
+            $worker->position->department->id != $user->position->department->id
         ) {
             return response('You don\'t have permission to view this user\'s data', 500);
         }
