ddb58f74750912ee5c3a8cdc1bbac1e442e44345
xWillQ <pasha.shevchenko.90@gmail.com>
2020-06-10 19:35:05 +0700

    Added voyager to the project

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(VoyagerDatabaseSeeder::class);
         $this->call(DepartmentSeeder::class);
         $this->call(PositionSeeder::class);
         $this->call(UserSeeder::class);
